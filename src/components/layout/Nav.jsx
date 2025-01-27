e270a48bf22a44fc66051e17afc2dd6e7d72a572
Calvin Hinze <calvinhinzer@gmail.com>
2025-01-27 12:12:57 +0100

    [FEATURE] navbar

---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,78 @@
+import React from "react"
+import styled from "styled-components"
+import { useAtom, useAtomValue } from "jotai"
+
+import { sizes } from "@/base/variables"
+
+import CDALogo from "@/assets/cda-logo-bw.svg"
+
+import { languageAtom } from "@/store/store.jsx"
+
+const StyledNav = styled.nav`
+  display: flex;
+  width: 100%;
+  padding: ${sizes.m} ${sizes.l};
+
+  ul {
+    display: flex;
+    column-gap: ${sizes.xxl};
+    align-items: center;
+    li {
+    }
+  }
+
+  .cda-logo-wrap {
+    width: 12.6rem;
+    padding: 0;
+    margin-right: -3px;
+    .cda-logo {
+      height: 1.2rem;
+      opacity: 0.3;
+    }
+  }
+  .apps-icon-wrap {
+    display: flex;
+    align-items: center;
+    column-gap: ${sizes.s};
+    text-decoration: none;
+    color: black;
+    .apps-icon {
+      height: 1.2rem;
+      opacity: 1;
+
+      margin-right: auto.36rem;
+    }
+  }
+`
+
+const Nav = () => {
+  const lang = useAtomValue(languageAtom)
+
+  const translations = {
+    en: "go to works search",
+    de: "zur Werksuche",
+  }
+
+  return (
+    <StyledNav>
+      <ul>
+        <li>
+          <a href="https://lucascranach.org/" className="cda-logo-wrap">
+            <img className="cda-logo" src={CDALogo} alt="CDA Logo" />
+          </a>
+        </li>
+        <li>
+          <a
+            href={`https://lucascranach.org/${lang}/search?kind=works&loadLatestSearchConfiguration=true`}
+            className="apps-icon-wrap"
+          >
+            <span class="material-icons">apps</span>
+            <span>{translations[lang]}</span>
+          </a>
+        </li>
+      </ul>
+    </StyledNav>
+  )
+}
+
+export default Nav
